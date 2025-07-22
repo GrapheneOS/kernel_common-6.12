534e24e53c03185c1a93d32ff81ff633e6d01087
Bart Van Assche <bvanassche@acm.org>
2025-07-22 13:35:56 -0700

    FROMLIST: blk-mq: Always insert sequential zoned writes into a software queue
    One of the optimizations in the block layer is that the software queues
    are bypassed if it is expected that the block driver will accept a
    request. This can cause request reordering even for requests submitted
    from the same CPU core.  This patch preserves the order for sequential
    zoned writes submitted from a given CPU core by always inserting these
    requests into the appropriate software queue.
    
    Cc: Damien Le Moal <dlemoal@kernel.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Bug: 234829282
    Bug: 449729210
    Link: https://lore.kernel.org/linux-block/20250827212937.2759348-3-bvanassche@acm.org/
    Change-Id: Ice462924edeccb00e1b77af65d1c8589fe001287
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -716,6 +716,18 @@ static inline unsigned int disk_nr_zones(struct gendisk *disk)
 	return disk->nr_zones;
 }
 
+/*
+ * blk_pipeline_zwr() - Whether or not sequential zoned writes will be
+ *	pipelined per zone.
+ * @q: request queue pointer.
+ *
+ * Return: %true if and only if zoned writes will be pipelined per zone.
+ */
+static inline bool blk_pipeline_zwr(struct request_queue *q)
+{
+	return q->limits.features & BLK_FEAT_ORDERED_HWQ;
+}
+
 /**
  * bio_needs_zone_write_plugging - Check if a BIO needs to be handled with zone
  *				   write plugging
@@ -769,6 +781,11 @@ bool blk_zone_plug_bio(struct bio *bio, unsigned int nr_segs);
 static inline unsigned int disk_nr_zones(struct gendisk *disk)
 {
 	return 0;
+}
+
+static inline bool blk_pipeline_zwr(struct request_queue *q)
+{
+	return false;
 }
 
 static inline bool bio_needs_zone_write_plugging(struct bio *bio)
