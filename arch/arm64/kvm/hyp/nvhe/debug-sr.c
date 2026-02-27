dfb02af8089713c595de9e3fb01cd97e0af0f599
Will Deacon <will@kernel.org>
2026-02-27 21:21:33 +0000

    BACKPORT: FROMLIST: KVM: arm64: Disable TRBE Trace Buffer Unit when running in guest context
    The nVHE world-switch code relies on zeroing TRFCR_EL1 to disable trace
    generation in guest context when self-hosted TRBE is in use by the host.
    
    Per D3.2.1 ("Controls to prohibit trace at Exception levels"), clearing
    TRFCR_EL1 means that trace generation is prohibited at EL1 and EL0 but
    per R_YCHKJ the Trace Buffer Unit will still be enabled if
    TRBLIMITR_EL1.E is set. R_SJFRQ goes on to state that, when enabled, the
    Trace Buffer Unit can perform address translation for the "owning
    exception level" even when it is out of context.
    
    Consequently, we can end up in a state where TRBE performs speculative
    page-table walks for a host VA/IPA in guest/hypervisor context depending
    on the value of MDCR_EL2.E2TB, which changes over world-switch. The
    potential result appears to be a heady mixture of SErrors, data
    corruption and hardware lockups.
    
    Extend the TRBE world-switch code to clear TRBLIMITR_EL1.E after
    draining the buffer, restoring the register on return to the host. This
    unfortunately means we need to tackle CPU errata #2064142 and #2038923
    which add additional synchronisation requirements around manipulations
    of the limit register. Hopefully this doesn't need to be fast.
    
    [tabba: Resolved conflict in arch/arm64/kvm/hyp/nvhe/debug-sr.c and
    switch.c caused by upstream refactoring around __debug_save_trace]
    
    Bug: 473214797
    Link: https://lore.kernel.org/all/20260227212136.7660-2-will@kernel.org/
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Oliver Upton <oupton@kernel.org>
    Cc: James Clark <james.clark@linaro.org>
    Cc: Leo Yan <leo.yan@arm.com>
    Cc: Suzuki K Poulose <suzuki.poulose@arm.com>
    Cc: Fuad Tabba <tabba@google.com>
    Cc: Alexandru Elisei <alexandru.elisei@arm.com>
    Fixes: a1319260bf62 ("arm64: KVM: Enable access to TRBE support for host")
    Change-Id: Icc9bac6390c5c59be2a4e558290d9db1013e544e
    Signed-off-by: Will Deacon <will@kernel.org>
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Tested-by: Leo Yan <leo.yan@arm.com>
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/kvm/hyp/nvhe/debug-sr.c
+++ arch/arm64/kvm/hyp/nvhe/debug-sr.c
@@ -51,29 +51,70 @@ static void __debug_restore_spe(u64 pmscr_el1)
 	write_sysreg_el1(pmscr_el1, SYS_PMSCR);
 }
 
-static void __debug_save_trace(u64 *trfcr_el1)
+static void __debug_save_trace(u64 *trfcr_el1, u64 *trblimitr_el1)
 {
-	*trfcr_el1 = 0;
+	*trblimitr_el1 = read_sysreg_s(SYS_TRBLIMITR_EL1);
 
 	/* Check if the TRBE is enabled */
-	if (!(read_sysreg_s(SYS_TRBLIMITR_EL1) & TRBLIMITR_EL1_E))
-		return;
-	/*
-	 * Prohibit trace generation while we are in guest.
-	 * Since access to TRFCR_EL1 is trapped, the guest can't
-	 * modify the filtering set by the host.
-	 */
-	*trfcr_el1 = read_sysreg_el1(SYS_TRFCR);
-	write_sysreg_el1(0, SYS_TRFCR);
-	isb();
-	/* Drain the trace buffer to memory */
-	tsb_csync();
+	if (*trblimitr_el1 & TRBLIMITR_EL1_E) {
+		/*
+		 * Prohibit trace generation while we are in guest.
+		 * Since access to TRFCR_EL1 is trapped, the guest can't
+		 * modify the filtering set by the host.
+		 */
+		*trfcr_el1 = read_sysreg_el1(SYS_TRFCR);
+		write_sysreg_el1(0, SYS_TRFCR);
+
+		/*
+		 * The host has enabled the Trace Buffer Unit so we have
+		 * to beat the CPU with a stick until it stops accessing
+		 * memory.
+		 */
+
+		/* First, ensure that our prior write to TRFCR has stuck. */
+		isb();
+
+		/* Now synchronise with the trace and drain the buffer. */
+		tsb_csync();
+		dsb(nsh);
+
+		/*
+		 * With no more trace being generated, we can disable the
+		 * Trace Buffer Unit.
+		 */
+		write_sysreg_s(0, SYS_TRBLIMITR_EL1);
+		if (cpus_have_final_cap(ARM64_WORKAROUND_2064142)) {
+			/*
+			 * Some CPUs are so good, we have to drain 'em
+			 * twice.
+			 */
+			tsb_csync();
+			dsb(nsh);
+		}
+
+		/*
+		 * Ensure that the Trace Buffer Unit is disabled before
+		 * we start mucking with the stage-2 and trap
+		 * configuration.
+		 */
+		isb();
+	}
 }
 
-static void __debug_restore_trace(u64 trfcr_el1)
+static void __debug_restore_trace(u64 trfcr_el1, u64 trblimitr_el1)
 {
-	if (!trfcr_el1)
-		return;
+	if (trblimitr_el1 & TRBLIMITR_EL1_E) {
+		/* Re-enable the Trace Buffer Unit for the host. */
+		write_sysreg_s(trblimitr_el1, SYS_TRBLIMITR_EL1);
+		isb();
+		if (cpus_have_final_cap(ARM64_WORKAROUND_2038923)) {
+			/*
+			 * Make sure the unit is re-enabled before we
+			 * poke TRFCR.
+			 */
+			isb();
+		}
+	}
 
 	/* Restore trace filter controls */
 	write_sysreg_el1(trfcr_el1, SYS_TRFCR);
@@ -86,7 +127,8 @@ void __debug_save_host_buffers_nvhe(struct kvm_vcpu *vcpu)
 		__debug_save_spe(host_data_ptr(host_debug_state.pmscr_el1));
 	/* Disable and flush Self-Hosted Trace generation */
 	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_TRBE))
-		__debug_save_trace(host_data_ptr(host_debug_state.trfcr_el1));
+		__debug_save_trace(host_data_ptr(host_debug_state.trfcr_el1),
+				   host_data_ptr(host_debug_state.trblimitr_el1));
 }
 
 void __debug_switch_to_guest(struct kvm_vcpu *vcpu)
@@ -99,7 +141,8 @@ void __debug_restore_host_buffers_nvhe(struct kvm_vcpu *vcpu)
 	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_SPE))
 		__debug_restore_spe(*host_data_ptr(host_debug_state.pmscr_el1));
 	if (vcpu_get_flag(vcpu, DEBUG_STATE_SAVE_TRBE))
-		__debug_restore_trace(*host_data_ptr(host_debug_state.trfcr_el1));
+		__debug_restore_trace(*host_data_ptr(host_debug_state.trfcr_el1),
+				      *host_data_ptr(host_debug_state.trblimitr_el1));
 }
 
 void __debug_switch_to_host(struct kvm_vcpu *vcpu)
